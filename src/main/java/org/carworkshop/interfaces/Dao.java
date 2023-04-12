403d83fe8cfdd3310f83874e8d392d66b32fac05
zenlaboratory <zenlaboratory@gmail.com>
2023-04-12 19:38:00 +0200

    Refactored Cita.java & Dao.java.

--- src/main/java/org/carworkshop/interfaces/Dao.java
+++ src/main/java/org/carworkshop/interfaces/Dao.java
@@ -1,9 +1,7 @@
 package org.carworkshop.interfaces;
 
-import jakarta.persistence.Query;
-import java.util.Optional;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface Dao<T> {
 
